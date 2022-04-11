fe5d9e1b387f50026d69d0b913531b3f05cf61d7
unknown <daksh.bagga2020@vitstudent.ac.in>
2022-04-11 16:31:01 +0530

    origin

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,21 +3,33 @@ import {CircularProgress, Grid, Typography, InputLabel, MenuItem,FormControl,Sel
 
 
 import useStyles from './styles'
+import { SettingsSystemDaydreamSharp } from '@material-ui/icons';
 
 const List = () => {
     const classes=useStyles();
-    
+    const [type,setType] = useState('restaurants');
+    const [rating,setRating] = useState('');
+
     return(
         <div className={classes.container}>
             <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
             <FormControl className={classes.formControl}>
                 <InputLabel>Type</InputLabel>
-                <Select value={} onChange={}>
+                <Select value={type} onChange={(e)=>setType(e.target.value)}>
                     <MenuItem value="restaurants">Restaurants</MenuItem>
                     <MenuItem value="hotels">Hotels</MenuItem>
                     <MenuItem value="attractions">Attractions</MenuItem>
                 </Select>
-
+            </FormControl>
+    
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e)=>setRating(e.target.value)}>
+                    <MenuItem value = {0}>All</MenuItem>
+                    <MenuItem value = {3}>Above 3.0 Stars</MenuItem>
+                    <MenuItem value = {4}>Above 4.0 Stars</MenuItem>
+                    <MenuItem value = {4.5}>Above 4.5 Stars</MenuItem>
+                </Select>
             </FormControl>
         </div>
 
